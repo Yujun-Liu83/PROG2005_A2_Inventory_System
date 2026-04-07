9cd5db24a6b71460e557365bf5e860c969e5ce2f
Liuyu_Jun <13784004+sixty-728@user.noreply.gitee.com>
2026-04-07 18:17:46 +0800

    Feat: Add statistics and feature cards to the home page, and optimize the display effect.

--- Part2_Angular/src/app/home/home.ts
+++ Part2_Angular/src/app/home/home.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { Inventory } from '../inventory';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+  totalItems = 0;
+  lowStockCount = 0;
+  popularCount = 0;
+
+  constructor(private inventoryService: Inventory) {}
+
+  ngOnInit(): void {
+    const items = this.inventoryService.getAllItems();
+    this.totalItems = items.length;
+    this.lowStockCount = items.filter(item => item.quantity < 5).length;
+    this.popularCount = items.filter(item => item.isPopular).length;
+  }
+}
